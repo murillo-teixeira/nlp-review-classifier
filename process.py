dda27c4cb397ad74c6f1c549c540c00cd3fc7831
hugo-ribeiro-36 <61517576+hugo-ribeiro-36@users.noreply.github.com>
2023-10-15 19:49:17 +0100

    Add files via upload

---
+++ process.py
@@ -0,0 +1,287 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import torch
+import seaborn as sns
+import transformers
+import json
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from transformers import RobertaModel, RobertaTokenizer
+import logging
+logging.basicConfig(level=logging.ERROR)
+
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
+
+colnames = ['Label', 'Values']
+colname = ['Values']
+
+
+train = pd.read_csv('output.csv', names=colnames, header=None)
+final = pd.read_csv('final.csv', names=colname, header=None)
+
+#train = pd.read_csv('output.csv', delimiter='\t')
+print(train.shape)
+#print(train.head())
+
+
+MAX_LEN = 256
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+# EPOCHS = 1
+LEARNING_RATE = 1e-05
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base', truncation=True, do_lower_case=True)
+
+
+class SentimentData(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.text = dataframe.Values
+        self.targets = self.data.Label
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, index):
+        text = str(self.text[index])
+        text = " ".join(text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
+    
+new_df = train[['Label', 'Values']]
+
+train_size = 0.70
+train_data = new_df.sample(frac=train_size,random_state=100)
+test_data = new_df.drop(train_data.index).reset_index(drop=True)
+train_data = train_data.reset_index(drop=True)
+
+
+print("FULL Dataset: {}".format(new_df.shape))
+print("TRAIN Dataset: {}".format(train_data.shape))
+print("TEST Dataset: {}".format(test_data.shape))
+
+training_set = SentimentData(new_df, tokenizer, MAX_LEN)
+testing_set = SentimentData(test_data, tokenizer, MAX_LEN)
+
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+
+class RobertaClass(torch.nn.Module):
+    def __init__(self):
+        super(RobertaClass, self).__init__()
+        self.l1 = RobertaModel.from_pretrained("roberta-base")
+        self.pre_classifier = torch.nn.Linear(768, 768)
+        self.dropout = torch.nn.Dropout(0.3)
+        self.classifier = torch.nn.Linear(768, 5)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        hidden_state = output_1[0]
+        pooler = hidden_state[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = torch.nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        return output
+    
+model = RobertaClass()
+model.to(device)
+
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+
+
+def calcuate_accuracy(preds, targets):
+    n_correct = (preds==targets).sum().item()
+    return n_correct
+
+
+
+def train(epoch):
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    model.train()
+    for _,data in tqdm(enumerate(training_loader, 0)):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
+
+        outputs = model(ids, mask, token_type_ids)
+        loss = loss_function(outputs, targets)
+        tr_loss += loss.item()
+        big_val, big_idx = torch.max(outputs.data, dim=1)
+        n_correct += calcuate_accuracy(big_idx, targets)
+
+        nb_tr_steps += 1
+        nb_tr_examples+=targets.size(0)
+        
+        if _%5000==0:
+            loss_step = tr_loss/nb_tr_steps
+            accu_step = (n_correct*100)/nb_tr_examples 
+            print(f"Training Loss per 5000 steps: {loss_step}")
+            print(f"Training Accuracy per 5000 steps: {accu_step}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        # # When using GPU
+        optimizer.step()
+
+    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Training Loss Epoch: {epoch_loss}")
+    print(f"Training Accuracy Epoch: {epoch_accu}")
+
+    return 
+
+
+EPOCHS = 5
+for epoch in range(EPOCHS):
+    train(epoch)
+
+
+def valid(model, testing_loader):
+    model.eval()
+    n_correct = 0; n_wrong = 0; total = 0; tr_loss=0; nb_tr_steps=0; nb_tr_examples=0
+    with torch.no_grad():
+        for _, data in tqdm(enumerate(testing_loader, 0)):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype = torch.long)
+            outputs = model(ids, mask, token_type_ids).squeeze()
+            loss = loss_function(outputs, targets)
+            tr_loss += loss.item()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            n_correct += calcuate_accuracy(big_idx, targets)
+
+            nb_tr_steps += 1
+            nb_tr_examples+=targets.size(0)
+            
+            if _%5000==0:
+                loss_step = tr_loss/nb_tr_steps
+                accu_step = (n_correct*100)/nb_tr_examples
+                print(f"Validation Loss per 100 steps: {loss_step}")
+                print(f"Validation Accuracy per 100 steps: {accu_step}")
+    epoch_loss = tr_loss/nb_tr_steps
+    epoch_accu = (n_correct*100)/nb_tr_examples
+    print(f"Validation Loss Epoch: {epoch_loss}")
+    print(f"Validation Accuracy Epoch: {epoch_accu}")
+    
+    return epoch_accu
+    
+
+acc = valid(model, testing_loader)
+print("Accuracy on test data = %0.2f%%" % acc)
+
+
+output_model_file = 'pytorch_roberta_sentiment.bin'
+output_vocab_file = './'
+
+model_to_save = model
+torch.save(model_to_save, output_model_file)
+tokenizer.save_vocabulary(output_vocab_file)
+
+print('All files saved')
+print('This tutorial is completed')
+
+
+
+new_data = pd.read_csv('final.csv', names=colname, header=None)
+new_texts = new_data['Values']
+
+
+class UnlabeledData(Dataset):
+    def __init__(self, text_list, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.text_list = text_list
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.text_list)
+
+    def __getitem__(self, index):
+        text = str(self.text_list[index])
+        text = " ".join(text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long)
+        }
+
+new_dataset = UnlabeledData(new_texts, tokenizer, MAX_LEN)
+new_data_params = {'batch_size': VALID_BATCH_SIZE, 'shuffle': False, 'num_workers': 0}
+new_data_loader = DataLoader(new_dataset, **new_data_params)
+
+def predict(model, data_loader):
+    model.eval()
+    predictions = []
+    with torch.no_grad():
+        for _, data in tqdm(enumerate(data_loader, 0)):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            outputs = model(ids, mask, token_type_ids).squeeze()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            predictions.extend(big_idx.tolist())
+    return predictions
+
+# Perform predictions on the new dataset
+predicted_labels = predict(model, new_data_loader)
+
+# Add the predicted labels to the new dataset
+new_data['Predicted_Labels'] = predicted_labels
+
+# Save the new dataset with predicted labels to a CSV file
+new_data.to_csv('new_data_with_labels.csv', index=False)
+
+print("Predictions saved to 'new_data_with_labels.csv'")
